f717681774d166e83e7e16b64f77087d6ea78842
Arthur RICHARD <arthur.richard2299@gmail.com>
2019-10-03 18:56:36 +0200

    1.0
    Stable

---
+++ BattaeryManager/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BattaeryManager
+{
+    partial class BatteryManager
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BatteryManager));
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.btn_low = new System.Windows.Forms.Button();
+            this.btn_high = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
+            this.Logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Logo.BackgroundImage")));
+            this.Logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Logo.Location = new System.Drawing.Point(164, 216);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(219, 49);
+            this.Logo.TabIndex = 0;
+            this.Logo.TabStop = false;
+            // 
+            // btn_low
+            // 
+            this.btn_low.BackColor = System.Drawing.Color.Gold;
+            this.btn_low.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_low.BackgroundImage")));
+            this.btn_low.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_low.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_low.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_low.Image = ((System.Drawing.Image)(resources.GetObject("btn_low.Image")));
+            this.btn_low.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_low.Location = new System.Drawing.Point(45, 39);
+            this.btn_low.Name = "btn_low";
+            this.btn_low.Size = new System.Drawing.Size(128, 130);
+            this.btn_low.TabIndex = 1;
+            this.btn_low.UseVisualStyleBackColor = false;
+            this.btn_low.Click += new System.EventHandler(this.btn_low_Click);
+            // 
+            // btn_high
+            // 
+            this.btn_high.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_high.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_high.BackgroundImage")));
+            this.btn_high.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_high.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_high.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_high.Location = new System.Drawing.Point(206, 39);
+            this.btn_high.Name = "btn_high";
+            this.btn_high.Size = new System.Drawing.Size(129, 130);
+            this.btn_high.TabIndex = 2;
+            this.btn_high.UseVisualStyleBackColor = false;
+            this.btn_high.Click += new System.EventHandler(this.btn_high_Click);
+            // 
+            // BatteryManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(378, 265);
+            this.Controls.Add(this.btn_high);
+            this.Controls.Add(this.btn_low);
+            this.Controls.Add(this.Logo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BatteryManager";
+            this.Text = "Battery Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.Button btn_low;
+        private System.Windows.Forms.Button btn_high;
+    }
+}
+
---
+++ BattaeryManager/Form1.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattaeryManager
+{
+    public partial class BatteryManager : Form
+    {
+        private static Guid GUID_SLEEP_SUBGROUP = new Guid("238c9fa8-0aad-41ed-83f4-97be242c8f20");
+        private static Guid GUID_HIBERNATEIDLE = new Guid("9d7815a6-7ee4-497e-8888-515a05f02364");
+        private static Guid SCHEME_MAX = new Guid("a1841308-3541-4fab-bc81-f71556f20b4a");
+        private static Guid SCHEME_BALANCED = new Guid("381b4222-f694-41f0-9685-ff5bb260df2e");
+        private static Guid SCHEME_MIN = new Guid("8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c");
+
+        [DllImport("GDI32.dll")]
+        private unsafe static extern bool SetDeviceGammaRamp(int hdc, void* ramp); 
+
+        [DllImport("powrprof.dll")]
+        static unsafe extern uint PowerSetActiveScheme(
+           IntPtr UserRootPowerKey,
+           ref Guid ActivePolicyGuid);
+
+        [DllImport("powrprof.dll")]
+        static unsafe extern uint PowerGetActiveScheme(
+           IntPtr UserRootPowerKey,
+           ref IntPtr ActivePolicyGuid);
+
+        [DllImport("powrprof.dll")]
+        static unsafe extern uint PowerReadACValue(
+           IntPtr RootPowerKey,
+           ref Guid SchemeGuid,
+           ref Guid SubGroupOfPowerSettingGuid,
+           ref Guid PowerSettingGuid,
+           ref int Type,
+           ref int Buffer,
+           ref uint BufferSize);
+        public BatteryManager()
+        {            
+            InitializeComponent();
+        }
+        private static bool initialized = false;
+        private static Int32 hdc;
+
+
+        private static void InitializeClass()
+        {
+            if (initialized)
+                return;
+
+            //Get the hardware device context of the screen, we can do
+            //this by getting the graphics object of null (IntPtr.Zero)
+            //then getting the HDC and converting that to an Int32.
+            hdc = Graphics.FromHwnd(IntPtr.Zero).GetHdc().ToInt32();
+
+            initialized = true;
+        }
+
+        public static unsafe bool SetBrightness(short brightness)
+        {
+            InitializeClass();
+
+            if (brightness > 255)
+                brightness = 255;
+
+            if (brightness < 0)
+                brightness = 0;
+
+            short* gArray = stackalloc short[3 * 256];
+            short* idx = gArray;
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    int arrayVal = i * (brightness + 128);
+
+                    if (arrayVal > 65535)
+                        arrayVal = 65535;
+
+                    *idx = (short)arrayVal;
+                    idx++;
+                }
+            }
+
+            //For some reason, this always returns false?
+            bool retVal = SetDeviceGammaRamp(hdc, gArray);
+
+            //Memory allocated through stackalloc is automatically free'd
+            //by the CLR.
+
+            return retVal;
+        }
+        private void btn_high_Click(object sender, EventArgs e)
+        { 
+            PowerSetActiveScheme(IntPtr.Zero, ref SCHEME_MIN);
+            //SetBrightness(150);
+            MessageBox.Show("mode Hautes Performances activé");
+        }
+
+        private void btn_low_Click(object sender, EventArgs e)
+        {
+            PowerSetActiveScheme(IntPtr.Zero, ref SCHEME_MAX);
+            //SetBrightness(75);
+            MessageBox.Show("mode Économie d'énergie activé");
+        }
+
+    }
+}
